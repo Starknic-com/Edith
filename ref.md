8b015b8acffe572e11293cacaec177fa52532322
arun.muraleedharan <arun.muraleedharan@flytxt.com>
2019-08-18 18:30:00 +0530

    more blocks

---
+++ ref.md
@@ -0,0 +1,3 @@
+http://www.mblock.cc/doc/en/block-reference/shape.html
+
+https://www.vernier.com/til/4141/
--- scratch-vm/src/extensions/pikachubot/index.js
+++ scratch-vm/src/extensions/pikachubot/index.js
@@ -1,10 +1,26 @@
-import {NUMBER} from '../../extension-support/argument-type';
-import {COMMAND} from '../../extension-support/block-type';
+/* eslint-disable camelcase */
+import ArgumentType from '../../extension-support/argument-type';
+import BlockType from '../../extension-support/block-type';
 import {log as _log} from '../../util/log';
 
 import CustomScratchLinkSocketFactory from './link-socket-factory';
 import {PikachuBotPeripheral} from './peripheral';
 
+
+const Motor = {
+    LEFT: 'LEFT',
+    RIGHT: 'RIGHT'
+};
+
+const Sensor = {
+    LEFT: 'LEFT',
+    RIGHT: 'RIGHT'
+};
+
+const SensorState = {
+    DARK: 'DARK',
+    LIGHT: 'LIGHT'
+};
 
 const blockIconURI = 'https://img.icons8.com/color/48/000000/pikachu-pokemon.png';
 export default class PikachuBotExtenstion {
@@ -42,23 +58,120 @@ export default class PikachuBotExtenstion {
             showStatusButton: true,
             blocks: [
                 {
-                    opcode: 'op_doBeep',
-                    blockType: COMMAND,
+                    opcode: this.op_beep.name,
+                    blockType: BlockType.COMMAND,
                     text: 'beep for [INTERVAL] sec',
                     arguments: {
                         INTERVAL: {
-                            type: NUMBER,
+                            type: ArgumentType.NUMBER,
                             defaultValue: 5
+                        }
+                    }
+                },
+                {
+                    opcode: this.op_move.name,
+                    blockType: BlockType.COMMAND,
+                    text: 'move left by [LEFT] and right by [RIGHT]',
+                    arguments: {
+                        LEFT: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 0
+                        },
+                        RIGHT: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 0
+                        }
+                    }
+                },
+                {
+                    opcode: this.op_setSpeed.name,
+                    blockType: BlockType.COMMAND,
+                    text: 'set motor [MOTOR] speed [SPEED]',
+                    arguments: {
+                        MOTOR: {
+                            type: ArgumentType.STRING,
+                            menu: 'motors',
+                            defaultValue: Motor.LEFT
+                        },
+                        SPEED: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 0
+                        }
+                    }
+                },
+                {
+                    opcode: this.op_isSensorStateMatched.name,
+                    blockType: BlockType.BOOLEAN,
+                    text: 'sensor [SENSOR] is [STATE]',
+                    arguments: {
+                        SENSOR: {
+                            type: ArgumentType.STRING,
+                            menu: 'sensors',
+                            defaultValue: Sensor.LEFT
+                        },
+                        STATE: {
+                            type: ArgumentType.STRING,
+                            menu: 'sensorStates',
+                            defaultValue: SensorState.LIGHT
                         }
                     }
                 }
             ],
             menus: {
+                motors: {
+                    acceptReporters: true,
+                    items: [{
+                        text: 'LEFT',
+                        value: Motor.LEFT
+                    },
+                    {
+                        text: 'RIGHT',
+                        value: Motor.RIGHT
+                    }]
+                },
+                sensors: {
+                    acceptReporters: true,
+                    items: [{
+                        text: 'LEFT',
+                        value: Sensor.LEFT
+                    },
+                    {
+                        text: 'RIGHT',
+                        value: Sensor.RIGHT
+                    }]
+                },
+                sensorStates: {
+                    acceptReporters: true,
+                    items: [{
+                        text: 'DARK',
+                        value: SensorState.DARK
+                    },
+                    {
+                        text: 'LIGHT',
+                        value: SensorState.LIGHT
+                    }]
+                }
             }
         };
     }
 
-    op_doBeep (args) {
+    op_move (args) {
+        return console.log(args);
+        this._peripheral.move(args.LEFT, args.RIGHT);
+    }
+
+    op_isSensorStateMatched (args){
+        return console.log(args);
+        return this._peripheral.sensorState(args.SENSOR) === args.STATE;
+    }
+
+    op_setSpeed (args) {
+        return console.log(args);
+        this._peripheral.setSpeed(args.MOTOR, args.SPEED);
+    }
+
+    op_beep (args) {
+        return console.log(args);
         this._peripheral.beep(args.INTERVAL);
     }
 }
