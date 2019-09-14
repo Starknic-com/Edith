fe097abf2ce25511b631bc6393897d91cd80f7ca
nom3ad <arunmj001@gmail.com>
2019-09-14 18:07:26 +0530

    firmware subproject added

--- firmware/main.py
+++ firmware/main.py
@@ -1,25 +1,26 @@
-import time
-import json
-import network
-import uasyncio
-import machine
+from time import time, sleep
+from json import dumps, loads
+from network import WLAN, STA_IF, AP_IF
+from uasyncio import get_event_loop, start_server
+from machine import Pin
 from uwsserver import WSReader, WSWriter
 
 known_ap_configs = [
     (b"mountdoom", b"oneringtorulethemall"),
     (b"Kings Broadband", b"Cartier2018"),
+    (b"bdaygift", b"17072017"),
 ]
 
 PeripheralConfig = {
     "NAME": "Pikachu",
     "ID": "PIKA007",
     "DISCOVERY_PORT": 7007,
-    "LINK_PORT": 7007,
+    "LINK_PORT": 7008,
     "IP": "255.255.255.255",  # dummy
 }
 
-sta_if = network.WLAN(network.STA_IF)
-ap_if = network.WLAN(network.AP_IF)
+sta_if = WLAN(STA_IF)
+ap_if = WLAN(AP_IF)
 
 
 def find_aps(sort_by_strength=True):
@@ -37,8 +38,8 @@ def find_aps(sort_by_strength=True):
 def try_connect_to_known_aps(timeout=30, scan_retry_interval=5, ap_connect_timeout=10):
     sta_if.disconnect()
     sta_if.active(True)  # if sta is not active
-    started = time.time()
-    while (time.time() - started) <= timeout and not sta_if.isconnected():
+    started = time()
+    while (time() - started) <= timeout and not sta_if.isconnected():
         print("Scanning for APs")
         ap_list = find_aps(sort_by_strength=True)
         ssid_list = [a[0] for a in ap_list]
@@ -48,7 +49,7 @@ def try_connect_to_known_aps(timeout=30, scan_retry_interval=5, ap_connect_timeo
                 available.append(apc)
         if not available:
             print("No known APs found. Retry after ", scan_retry_interval, "sec")
-            time.sleep(scan_retry_interval)
+            sleep(scan_retry_interval)
             continue
         for apc in available:
             print("Connecting to ", apc)
@@ -58,7 +59,7 @@ def try_connect_to_known_aps(timeout=30, scan_retry_interval=5, ap_connect_timeo
 
 
 def connect_to_ap(essid, passwd, timeout=10):
-    started = time.time()
+    started = time()
     if sta_if.isconnected():
         if sta_if.config("essid") == essid:
             print("Already connected to ", sta_if.config("essid"))
@@ -68,16 +69,16 @@ def connect_to_ap(essid, passwd, timeout=10):
     sta_if.active(True)  # if sta is not active
     print("sta_if.connect()")
     sta_if.connect(essid, passwd)
-    while (time.time() - started) <= timeout:
+    while (time() - started) <= timeout:
         print("Waiting for connect")
-       sleep(2)
+        sleep(2)
         if sta_if.isconnected():
             print("Connected to ", essid.decode())
             return True
 
 
 def discovery_reply():
-    return json.dumps(PeripheralConfig).encode()
+    return dumps(PeripheralConfig).encode()
 
 
 async def start_discovery_server(ip, port):
@@ -109,7 +110,7 @@ async def start_discovery_server(ip, port):
         print(
             "UDP exchange completed"
         )  # TODO(mj) remove this comment, if implimentaion is OK
-       sleep(0.5)
+        sleep(0.5)
 
 
 pins = [0, 2, 4, 5, 12, 13, 14, 15, 16]
@@ -119,17 +120,17 @@ pins = [0, 2, 4, 5, 12, 13, 14, 15, 16]
 def get_state_reply():
     state = []
     for p in pins:
-        v = machine.Pin(p).value()
+        v = Pin(p).value()
         state.append(p)
         state.append(v)
-    return b"@S" + json.dumps(state)
+    return b"@S" + dumps(state)
 
 
 def set_pins(values):
     try:
         for i in range(1, len(values), 2):  # 1, 3, 5... upto n
             pin, val = i - 1, i
-            machine.Pin(pin).value(val)
+            Pin(pin).value(val)
         return b"@K"
     except Exception as oops:
         print("Error in setstate", oops)
@@ -159,7 +160,7 @@ async def link_endpoint_handler(reader, writer):
                 if data == GET_STATE_OP:
                     reply = get_state_reply()
                 elif data[:OP_LEN] == SET_PINS_OP:
-                    values = json.loads(OP_LEN)
+                    values = loads(OP_LEN)
                     reply = set_pins(values)
                 else:
                     reply = data
@@ -175,13 +176,13 @@ def main():
 
     print("Wait until connected to known APs")
     while not try_connect_to_known_aps():
-        time.sleep(5)
+        sleep(5)
         pass
 
     print("Network config:", sta_if.ifconfig())
     PeripheralConfig["IP"], *_ = sta_if.ifconfig()
 
-    loop = uasyncio.get_event_loop()
+    loop = get_event_loop()
 
     print(
         "Creating discovery server udp://{IP}:{DISCOVERY_PORT}".format(
@@ -196,10 +197,14 @@ def main():
 
     print("Creating websocket server tcp://{IP}:{LINK_PORT}".format(**PeripheralConfig))
     loop.create_task(
-        uasyncio.start_server(
+        start_server(
             link_endpoint_handler, PeripheralConfig["IP"], PeripheralConfig["LINK_PORT"]
         )
     )
+
+    import gc
+
+    gc.collect()
 
     print("Eventloop: run_forever()")
     loop.run_forever()
