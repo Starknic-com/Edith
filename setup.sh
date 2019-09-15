a45fbd67e5bc024bb3475219f883a13cfe5671d0
nom3ad <arunmj001@gmail.com>
2019-09-15 12:08:47 +0530

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-cd scratch-vm
+
+
+cd edith-link
+echo "installing edith-link deps"
+npm install
+
+cd ../scratch-vm
 echo "installing scratch-vm deps"
 npm install
 echo "linking scratch-vm to global prefix"
 npm link
+
 cd ../scratch-gui
 echo "installing scratch-gui deps"
 npm install
